6c4739f81e4b37018171601ae430933583ccee41
Krinal Mer <88674635+krinal2709@users.noreply.github.com>
2022-09-27 18:01:08 +0530

    first commit

---
+++ first.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+ class first {
+public static void main(String[] args) {
+    
+}
+    
+}
---
+++ six.java
@@ -0,0 +1,18 @@
+import java.util.*;
+ public class six{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Number :  ");
+        int n=sc.nextInt();
+        System.out.println();
+        for(int i=1; i<=n; i++ ){
+
+            for(int j=1; j<=i; j++){
+
+                System.out.print(j +" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
