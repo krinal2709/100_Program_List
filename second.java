de03458dab3adddca758f39d9cecb22ed58b3cba
Krinal Mer <88674635+krinal2709@users.noreply.github.com>
2022-10-02 16:20:55 +0530

    Update second.java

--- second.java
+++ second.java
@@ -1,28 +1,20 @@
 // whether string is palindrome or not.
 import java.util.*;
-
 class second {
-    static boolean isPalindrom(String str){
-        String rev="";
-        boolean ans=false;
-        for(int i=str.length()-1; i>=0; i--){
-            rev=rev+str.charAt(i);
-        if(str.equals(rev)){
-            ans=true;
-        }
-        }
-        return ans;
-    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter The String : ");
+        System.out.print("Enter The String :  ");
         String str=sc.nextLine();
         str=str.toLowerCase();
-        if(isPalindrom(str)){
-            System.out.println("Given String is Palindrome");
+        String rev=""; //empty rev string
+        for(int i=str.length()-1; i>=0; i--){
+            rev+=str.charAt(i);
+        }
+        if (str.equals(rev)) {
+            System.out.println("String is Palindrom");
         }
         else{
-            System.out.println("String is not palindrome");
-        }
+            System.out.println("String is Not Palindrom");
         }
     }
+}
